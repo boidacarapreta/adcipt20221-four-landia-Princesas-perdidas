9ff4d185a06310098f7ea9b2382e962daab4599f
radunzz <paula.rn@aluno.ifsc.edu.br>
2022-06-18 19:02:44 +0000

    Socket OK (eu acho)

--- cliente/js/cena1.js
+++ cliente/js/cena1.js
@@ -14,14 +14,18 @@ var trilha;
 var cameras;
 var parede;
 var labirinto;
-var A;
-var W;
-var S;
-var D;
 var vida;
 var placarVida;
 var gameOver;
+var jogador;
 var socket;
+var ice_servers = {
+  iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+};
+var localConnection;
+var remoteConnection;
+var midias;
+const audio = document.querySelector("audio");
 
 cena1.preload = function () {
   this.load.image("tileset0", "assets/terreno.png");
@@ -58,7 +62,155 @@ cena1.create = function () {
   //trilha = this.sound.add("musiquinha");
   //trilha.play();
   //trilha.setLoop(true);
+  parede = this.sound.add("efeito");
 
+  map = this.make.tilemap({ key: "map" });
+
+  // Primeira "tileset0" é o nome que está no tiled - Segundo "tileset0" é a key do preload
+  tileset0 = map.addTilesetImage("tileset0", "tileset0");
+
+  chao = map.createStaticLayer("chao", tileset0, 0, 0);
+
+  // Jogador 1 - controles/animação
+  player1 = this.physics.add.sprite(100, 90, "bruxa");
+  //player1.setCollideWorldBounds(true);
+
+  //  Animação do player1
+  this.anims.create({
+    key: "down",
+    frames: this.anims.generateFrameNumbers("bruxa", { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+  this.anims.create({
+    key: "left",
+    frames: this.anims.generateFrameNumbers("bruxa", { start: 4, end: 7 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+  this.anims.create({
+    key: "turn",
+    frames: [{ key: "bruxa", frame: 0 }],
+    frameRate: 20,
+  });
+  this.anims.create({
+    key: "right",
+    frames: this.anims.generateFrameNumbers("bruxa", { start: 8, end: 11 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+  this.anims.create({
+    key: "up",
+    frames: this.anims.generateFrameNumbers("bruxa", { start: 12, end: 15 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  // Jogador 2 - controles/animação
+  player2 = this.physics.add.sprite(150, 90, "branca");
+  //player2.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: "S",
+    frames: this.anims.generateFrameNumbers("branca", {
+      start: 0,
+      end: 3,
+    }),
+    frameRate: 10,
+    repeat: -1,
+  });
+  this.anims.create({
+    key: "A",
+    frames: this.anims.generateFrameNumbers("branca", {
+      start: 4,
+      end: 7,
+    }),
+    frameRate: 10,
+    repeat: -1,
+  });
+  this.anims.create({
+    key: "turn2",
+    frames: [{ key: "branca", frame: 0 }],
+    frameRate: 20,
+  });
+  this.anims.create({
+    key: "D",
+    frames: this.anims.generateFrameNumbers("branca", {
+      start: 8,
+      end: 11,
+    }),
+    frameRate: 10,
+    repeat: -1,
+  });
+  this.anims.create({
+    key: "W",
+    frames: this.anims.generateFrameNumbers("branca", {
+      start: 12,
+      end: 15,
+    }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  W = this.input.keyboard.addKey("W");
+  S = this.input.keyboard.addKey("S");
+  A = this.input.keyboard.addKey("A");
+  D = this.input.keyboard.addKey("D");
+
+  // Camada 1: terreno
+  labirinto = map.createStaticLayer("labirinto", tileset0, 0, 0);
+  labirinto.setCollisionByProperty({ collides: true });
+
+  // Cena (960x960) maior que a tela (800x600)
+  this.cameras.main.setBounds(0, 0, 1280, 800);
+  this.physics.world.setBounds(0, 0, 1280, 800);
+  this.cameras.main.setZoom(1);
+
+  // Câmera seguindo o personagem 1
+  //this.cameras.main.startFollow(player1);
+  //
+
+  //Tela cheia
+  var button2 = this.add //mudar onde o sprite fica para conseguir colocar no zoom 2
+    .image(100 - 16, 450, "telacheia", 0)
+    .setOrigin(1, 0)
+    .setInteractive()
+    .setScrollFactor(0);
+
+  // Ao clicar no botão de tela cheia
+  button2.on(
+    "pointerup",
+    function () {
+      if (this.scale.isFullscreen) {
+        button2.setFrame(0);
+        this.scale.stopFullscreen();
+      } else {
+        button2.setFrame(1);
+        this.scale.startFullscreen();
+      }
+    },
+    this
+  );
+
+  vida = 5;
+  placarVida = this.add.sprite(900 - 16, 50, "vida", 0).setScrollFactor(0);
+
+  cursors = this.input.keyboard.createCursorKeys();
+
+  var physics = this.physics;
+  
+  physics.add.collider(player1, tileset0);
+  physics.add.collider(player2, tileset0);
+  physics.add.collider(player1, player2, hitPlayer, null, this);
+
+  //worldLayer.setCollisionBetween();
+  //worldLayer.setCollisionByProperty({ collides: true });
+
+  // Detecção de colisão e disparo de evento: ARCas
+  //physics.add.collider(player1, labirinto, hitTiles, null, this);
+  //physics.add.collider(player2, labirinto, hitTiles, null, this);
+  physics.add.overlap(player1, player2, hitPlayer, null, this);
+  
   socket = io("https://still-tundra-75872.herokuapp.com/");
 
   socket.on("jogadores", (jogadores) => {
@@ -70,10 +222,7 @@ cena1.create = function () {
       player1.setCollideWorldBounds(true);
 
       // Detecção de colisão: terreno
-      physics.add.collider(player1, terreno, hitCave, null, this);
-
-      // Detecção de colisão e disparo de evento: ARCas
-      physics.add.collider(player1, ARCas, hitARCa, null, this);
+      physics.add.collider(player1, labirinto, hitTiles, null, this);
 
       // Câmera seguindo o personagem 1
       cameras.main.startFollow(player1);
@@ -92,10 +241,7 @@ cena1.create = function () {
       player2.setCollideWorldBounds(true);
 
       // Detecção de colisão: terreno
-      physics.add.collider(player2, terreno, hitCave, null, this);
-
-      // Detecção de colisão e disparo de evento: ARCas
-      physics.add.collider(player2, ARCas, hitARCa, null, this);
+      physics.add.collider(player2, labirinto, hitTiles, null, this);
 
       // Câmera seguindo o personagem 2
       cameras.main.startFollow(player2);
@@ -185,156 +331,6 @@ cena1.create = function () {
       player1.y = y;
     }
   });
-
-
-  parede = this.sound.add("efeito");
-
-  map = this.make.tilemap({ key: "map" });
-
-  // Primeira "tileset0" é o nome que está no tiled
-  // Segundo "tileset0" é a key do preload
-  tileset0 = map.addTilesetImage("tileset0", "tileset0");
-
-  chao = map.createStaticLayer("chao", tileset0, 0, 0);
-
-  // Jogador 1 - controles/animação
-  player1 = this.physics.add.sprite(100, 90, "bruxa");
-  player1.setCollideWorldBounds(true);
-
-  //  Animação do player1
-  this.anims.create({
-    key: "down",
-    frames: this.anims.generateFrameNumbers("bruxa", { start: 0, end: 3 }),
-    frameRate: 10,
-    repeat: -1,
-  });
-  this.anims.create({
-    key: "left",
-    frames: this.anims.generateFrameNumbers("bruxa", { start: 4, end: 7 }),
-    frameRate: 10,
-    repeat: -1,
-  });
-  this.anims.create({
-    key: "turn",
-    frames: [{ key: "bruxa", frame: 0 }],
-    frameRate: 20,
-  });
-  this.anims.create({
-    key: "right",
-    frames: this.anims.generateFrameNumbers("bruxa", { start: 8, end: 11 }),
-    frameRate: 10,
-    repeat: -1,
-  });
-  this.anims.create({
-    key: "up",
-    frames: this.anims.generateFrameNumbers("bruxa", { start: 12, end: 15 }),
-    frameRate: 10,
-    repeat: -1,
-  });
-
-  // Jogador 2 - controles/animação
-  player2 = this.physics.add.sprite(150, 90, "branca");
-  player2.setCollideWorldBounds(true);
-
-  this.anims.create({
-    key: "S",
-    frames: this.anims.generateFrameNumbers("branca", {
-      start: 0,
-      end: 3,
-    }),
-    frameRate: 10,
-    repeat: -1,
-  });
-  this.anims.create({
-    key: "A",
-    frames: this.anims.generateFrameNumbers("branca", {
-      start: 4,
-      end: 7,
-    }),
-    frameRate: 10,
-    repeat: -1,
-  });
-  this.anims.create({
-    key: "turn2",
-    frames: [{ key: "branca", frame: 0 }],
-    frameRate: 20,
-  });
-  this.anims.create({
-    key: "D",
-    frames: this.anims.generateFrameNumbers("branca", {
-      start: 8,
-      end: 11,
-    }),
-    frameRate: 10,
-    repeat: -1,
-  });
-  this.anims.create({
-    key: "W",
-    frames: this.anims.generateFrameNumbers("branca", {
-      start: 12,
-      end: 15,
-    }),
-    frameRate: 10,
-    repeat: -1,
-  });
-
-  W = this.input.keyboard.addKey("W");
-  S = this.input.keyboard.addKey("S");
-  A = this.input.keyboard.addKey("A");
-  D = this.input.keyboard.addKey("D");
-
-  // Camada 1: terreno
-  labirinto = map.createStaticLayer("labirinto", tileset0, 0, 0);
-  labirinto.setCollisionByProperty({ collides: true });
-
-  // Cena (960x960) maior que a tela (800x600)
-  this.cameras.main.setBounds(0, 0, 1280, 800);
-  this.physics.world.setBounds(0, 0, 1280, 800);
-
-  // Câmera seguindo o personagem 1
-  this.cameras.main.startFollow(player1);
-  this.cameras.main.setZoom(1);
-
-  //Tela cheia
-  var button2 = this.add //mudar onde o sprite fica para conseguir colocar no zoom 2
-    .image(100 - 16, 450, "telacheia", 0)
-    .setOrigin(1, 0)
-    .setInteractive()
-    .setScrollFactor(0);
-
-  // Ao clicar no botão de tela cheia
-  button2.on(
-    "pointerup",
-    function () {
-      if (this.scale.isFullscreen) {
-        button2.setFrame(0);
-        this.scale.stopFullscreen();
-      } else {
-        button2.setFrame(1);
-        this.scale.startFullscreen();
-      }
-    },
-    this
-  );
-
-  vida = 5;
-  placarVida = this.add.sprite(900 - 16, 50, "vida", 0).setScrollFactor(0);
-
-  var physics = this.physics;
-
-  cursors = this.input.keyboard.createCursorKeys();
-
-  this.physics.add.collider(player1, tileset0);
-  this.physics.add.collider(player2, tileset0);
-  this.physics.add.collider(player1, player2, hitPlayer, null, this);
-
-  //worldLayer.setCollisionBetween();
-  //worldLayer.setCollisionByProperty({ collides: true });
-
-  // Detecção de colisão e disparo de evento: ARCas
-  physics.add.collider(player1, labirinto, hitTiles, null, this);
-  physics.add.collider(player2, labirinto, hitTiles, null, this);
-  physics.add.overlap(player1, player2, hitPlayer, null, this);
 };
 
 cena1.update = function () {
